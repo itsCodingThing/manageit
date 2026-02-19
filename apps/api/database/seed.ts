e59884e68e3301b4b73d1bb2e1f5e75eb519b6f2
bhanu pratap singh <bhanuparitapsingh09@gmai.com>
2026-02-19 20:09:21 +0530

    added dev db seed

---
+++ apps/api/database/seed.ts
@@ -0,0 +1,29 @@
+import {db} from "@/database/db";
+import { AppType } from "@/app";
+import { betterAuthApi } from "@/services/auth"
+import { treaty } from "@elysiajs/eden";
+import { adminTable } from "./schema";
+
+const app = treaty<AppType>("localhost:3000");
+
+const admins = Array.from({length: 100}).map(async (_,i) => {
+  console.log("admin created: ", i);
+
+  const res = await betterAuthApi.signUpEmail({
+    body: {
+      name: `admin_${i}`,
+      email: `admin${i}@test.com`,
+      password: "admin123"
+    }
+  });
+
+  await db.insert(adminTable).values({
+      userId: res.user.id,
+      name: res.user.name,
+      email: res.user.email,
+      phoneNumber: res.user.phoneNumber ?? "9876543210"
+    });
+})
+
+await Promise.all(admins);
+console.log("admins created")
