2988eaa16f3a7198eb39ddd7cf2739d4e850a3ed
bhanu pratap singh <bhanuparitapsingh09@gmai.com>
2026-02-19 21:29:52 +0530

    update seed file

--- apps/api/database/seed.ts
+++ apps/api/database/seed.ts
@@ -1,38 +1,68 @@
-import {db} from "@/database/db";
+import { db } from "@/database/db";
 import { AppType } from "@/app";
-import { betterAuthApi } from "@/services/auth"
+import { betterAuthApi } from "@/services/auth";
 import { treaty } from "@elysiajs/eden";
 import { adminTable } from "./schema";
+import { faker } from "@faker-js/faker";
 
 const app = treaty<AppType>("localhost:3000");
 
-const admins = Array.from({length: 100}).map(async (_,i) => {
-  console.log("admin created: ", i);
+const admins = Array.from({ length: 100 }).map(async (_, i) => {
+	const res = await betterAuthApi.signUpEmail({
+		body: {
+			name: `admin_${i}`,
+			email: `admin${i}@test.com`,
+			password: "admin123",
+		},
+	});
 
-  const res = await betterAuthApi.signUpEmail({
-    body: {
-      name: `admin_${i}`,
-      email: `admin${i}@test.com`,
-      password: "admin123"
-    }
-  });
+	await db.insert(adminTable).values({
+		userId: res.user.id,
+		name: res.user.name,
+		email: res.user.email,
+		phoneNumber: res.user.phoneNumber ?? "9876543210",
+	});
+});
 
-  await db.insert(adminTable).values({
-      userId: res.user.id,
-      name: res.user.name,
-      email: res.user.email,
-      phoneNumber: res.user.phoneNumber ?? "9876543210"
-    });
-})
+const schools = async () => {
+	const res = await betterAuthApi.signInEmail({
+		body: {
+			email: "admin1@test.com",
+			password: "admin123",
+		},
+	});
 
-const schools = Array.from({length: 100}).map(async (_,i) => {
-  const res = await betterAuthApi.signInEmail({body: {
-    email: "admin1@test.com",
-    password: "admin123"
-  }});
+	return Array.from({ length: 100 }).map(async (_, i) => {
+		const mail = `school${i}@test.com`;
+		const phone = faker.phone.number({ style: "international" });
 
+		app.api.school.post(
+			{
+				schoolCode: i.toString(),
+				name: mail,
+				email: mail,
+				phone: phone,
+				address: faker.location.streetAddress(),
+				adminEmail: mail,
+				adminPassword: mail,
+				adminPhone: phone,
+				adminName: mail,
+				city: faker.location.city(),
+				country: faker.location.country(),
+				state: faker.location.state(),
+				postalCode: faker.location.zipCode(),
+			},
+			{
+				headers: {
+					authorization: `Bearer ${res.token}`,
+				},
+			},
+		);
+	});
+};
 
-})
-
+console.log("seeding database");
 await Promise.all(admins);
-console.log("admins created")
+const s = await schools();
+await Promise.all(s);
+console.log("seeded database");
